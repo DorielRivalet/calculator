02bd6b8c15ed78cf2e1d6b238dd6b315815252cd
DorielRivalet <100863878+DorielRivalet@users.noreply.github.com>
2022-05-10 04:31:39 -0300

    Implement custom fonts

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 const config = {
-  "OPERATOR_REGEX": /([\+\×\-\÷\%\^]{1})/g,
+  "OPERATOR_REGEX": /([\+\×\x\*\-\÷\/\%\^]{1})/g,
   "INITIAL_INPUT_VALUE": "",
   "INITIAL_RESULT_VALUE": 0,
-  "INITIAL_SCREEN_TEXT_COLOR": "00000096",
+  "INITIAL_SCREEN_TEXT_COLOR": "#000000",
   "INITIAL_SCREEN_BACKGROUND_COLOR": "#d2ff8f",
   "HISTORY_LOG_INITIAL_TEXT": "Enter equations with two operands to see past results!"
 };
